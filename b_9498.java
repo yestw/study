b63f73d80c6d7c0b6d0c16039cf781ab977134dd
yestw <shxodnjs54@naver.com>
2022-04-28 23:32:07 +0900

    백준9498번문제

---
+++ b_9498.java
@@ -0,0 +1,18 @@
+// 시험성적
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        if (num <= 100 && num >= 90)
+            System.out.println("A");
+        else if (num < 90 && num >= 80)
+           System.out.println("B");
+        else if (num < 80 && num >= 70)
+            System.out.println("C");
+        else if (num < 70 && num >= 60)
+            System.out.println("D");
+        else
+            System.out.println("F");
+    }
+}
