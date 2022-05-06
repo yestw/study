2b43e386d90324d00e34cd92966614aec6d17963
yestw <shxodnjs54@naver.com>
2022-05-06 09:33:24 +0900

    백준 15596번 문제 정수n개의 합

---
+++ b_15596.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//정수 N개의 합
+
+public class b_15596 {
+
+	static long sum(int[] a) {		
+		long sum = 0;
+		for (int i = 0; i < a.length; i++)
+		{
+			sum += a[i];
+		}
+		return  sum;
+	}
+	
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);		
+		int num = s.nextInt();
+		
+		int[] nums = new int[num];
+		for (int i = 0; i< nums.length; i++ ) {
+			nums[i] = s.nextInt();
+		}
+		System.out.println(sum(nums));
+	}
+}
