79c33326baabd4cc1f873f80d46c18e68c2d9f57
태원 <103083087+yestw@users.noreply.github.com>
2022-06-05 11:09:41 +0900

    [백준] 11654문제 아스키 코드

---
+++ 5.22/b_11654.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+      
+        Scanner input = new Scanner(System.in);
+        
+        
+        char a = input.nextLine().charAt(0);
+        
+        System.out.print((int)a);
+    }
+}
