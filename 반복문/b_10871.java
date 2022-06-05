2e7579097c24d2849d8df88cdc9a473c914830af
태원 <103083087+yestw@users.noreply.github.com>
2022-06-05 10:45:26 +0900

    [백준] 반복문 - 10871문제 X보다 작은 수

---
+++ 반복문/b_10871.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class b_10871 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int num = s.nextInt();
+		int num2 = s.nextInt();
+		
+		for(int i = 0; i < num; i++) {
+			int num3 = s.nextInt();
+			if(num2 >num3)
+				System.out.print(num3+" ");
+		}
+	}
+}
