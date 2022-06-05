8b2312c7e077f94573a3a420ca494184d9999cc6
태원 <103083087+yestw@users.noreply.github.com>
2022-06-05 10:43:59 +0900

    [백준] 반복문 - 2438문제 별 찍기 - 1

---
+++ 반복문/b_2438.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+        Scanner s = new Scanner(System.in);
+      
+        int num = Integer.parseInt(s.nextLine());
+        for(int i = 0; i < num; i++) {
+            for(int j = 0; j < num; j++) {
+                if(i >= j)
+                    System.out.print("*");
+            }
+             System.out.println();
+        }
+    }
+}
