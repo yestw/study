f74761b7a3ace4a4e015aaf38c3ba27dd9dc9a5c
태원 <103083087+yestw@users.noreply.github.com>
2022-06-05 10:47:12 +0900

    [백준] 반복문 - 1110문제 더하기 사이클

---
+++ 반복문/b_1110.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//더하기 사이클
+
+public class b_1110 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt(); //26
+		int n4 = n; //26
+		int count = 0;
+		
+		do {		
+			n =((n%10)*10) + ((n/10+n%10)%10) ;	//8
+			
+			count+=1;
+		} while (n4 != n);
+		
+		System.out.println(count);
+	}
+}
