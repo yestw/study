2e3b941218316459d132359767556872c9009147
태원 <103083087+yestw@users.noreply.github.com>
2022-06-05 10:49:03 +0900

    [백준] 배열- 2562문제 최댓값

---
+++ 배열/b_2562.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//최댓값
+
+public class b_2562 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int[] a = new int[9];
+		int max = 0;
+		int maxth = 0;
+		
+		
+		for(int i = 0; i < a.length; i++) {
+			a[i] = Integer.parseInt(s.nextLine());
+			if(a[i] > max) {
+				max = a[i];
+				maxth = i+1;
+			}
+		}
+		System.out.println(String.format("%d %d", max, maxth));
+	}
+}
