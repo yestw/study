51537e41c59cb689bc40a51420995c32d107f9df
태원 <103083087+yestw@users.noreply.github.com>
2022-07-18 21:50:09 +0900

    백준 1152번 문제 - 단어의 개수

---
+++ 알고리즘/b_1152.java
@@ -0,0 +1,15 @@
+//단어의 개수
+
+import java.io.*;
+import java.util.*;
+
+public class b_1152 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println((st.countTokens()));
+	}
+}
